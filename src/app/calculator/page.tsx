4e47ff6956fdd6ba92d37b1ca86fcf166a7f28c1
Innocent Aballa <in130014723@kisiiuniversity.ac.ke>
2026-06-27 14:02:21 +0300

    remove counter and calculator from ecommerce project, moved to mini-apps

--- src/app/calculator/page.tsx
+++
@@ -1,5 +0,0 @@
-import Calculator from "@/components/ui/Calculator/Calculator";
-
-export default function CalculatorPage() {
-    return <Calculator />;
-}
--- src/app/counter/page.tsx
+++
@@ -1,5 +0,0 @@
-import Counter from "@/components/ui/Counter/Counter";
-
-export default function CounterPage() {
-    return <Counter />;
-}
